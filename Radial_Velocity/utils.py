c88f0a46e47a1ff1cedefc44ce050221f00d8211
Jayshil <jayshilpatel147@gmail.com>
2021-03-03 21:23:47 +0530

    Added natural sorting function

--- Radial_Velocity/utils.py
+++ Radial_Velocity/utils.py
@@ -5,6 +5,7 @@ from scipy.optimize import curve_fit as cft
 import dfitspy as dft
 from astropy.io import fits
 import os
+import re
 
 def cubic(x, a, b, c, d):
     cc = a + b*x + c*(x**2) + d*(x**3)
@@ -12,3 +13,17 @@ def cubic(x, a, b, c, d):
 
 def line(x, m, c):
     return m*x + c
+
+#------------------------------------------------------------------------------------------
+#-------------------------------Natural Sorting--------------------------------------------
+#------------------------------------------------------------------------------------------
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
