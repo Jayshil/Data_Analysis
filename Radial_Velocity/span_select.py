e49978258c390d2604ff70df87fdb74a1a776d71
Jayshil <jayshilpatel147@gmail.com>
2021-03-05 20:04:46 +0530

    Script to select a region and fit Gaussian to it

---
+++ Radial_Velocity/span_select.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import SpanSelector
+import os
+from pathlib import Path
+from astropy.nddata import CCDData
+import ccdproc as ccdp
+from astropy.stats import mad_std
+import utils as utl
+import astropy.units as u
+import shutil
