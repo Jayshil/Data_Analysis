85fb876b22838a5f67645f656dcbd5e61db08ee2
Jayshil <jayshilpatel147@gmail.com>
2021-03-05 23:24:45 +0530

    Finding position of lines

--- Radial_Velocity/span_select.py
+++ Radial_Velocity/span_select.py
@@ -9,8 +9,11 @@ from astropy.stats import mad_std
 import utils as utl
 import astropy.units as u
 import shutil
+from scipy.optimize import minimize as mz
 
-def selection(flux, error_flx):
+def selection(loc, flux_file):
+    # Reading the data
+    pix, fl, fle = np.loadtxt(loc + flux_file, usecols=(0,1,2), unpack=True)
     # ---------------------------------------
     # To select the region of spectral lines
     # ---------------------------------------
@@ -44,6 +47,54 @@ def selection(flux, error_flx):
     # set useblit True on gtkagg for enhanced performance
     span = SpanSelector(ax, onselect, 'horizontal', useblit=True, 
                          rectprops=dict(alpha=0.5, facecolor='red'))
-
     plt.show()
-    return selection
+
+    mid_pix = []
+
+    for i in range(len(selection)):
+        aa = int(selection[i][0])
+        bb = int(selection[i][1])
+        pix1 = pix[aa:bb]
+        fl1 = fl[aa:bb]
+        fle1 = fle[aa:bb]
+        #xinit = np.array([fl1[0], 1, 1, 1])
+        xinit = np.array([(pix1[0] + pix1[-1])/2, 1, 1, 1])
+        def min_log_likelihood(x):
+            #model = utl.cubic(pix1, x[0], x[1], x[2], x[3])
+            model = utl.neg_gaus(pix1, x[0], x[1], x[2], x[3])
+            chi2 = (fl1 - model)/fle1
+            chi22 = np.sum(chi2**2)
+            yy = np.sum(np.log(fle1)) + 0.5*chi22
+            return yy
+        soln = mz(min_log_likelihood, xinit, method='L-BFGS-B')
+        #"""
+        mini = np.min(fl1)
+        mini1 = np.where(fl1 == mini)
+        mid_pix.append(pix1[mini1[0][0]])
+        #"""
+        #mid_pix.append(soln.x[0])
+        #plt.errorbar(pix, fl, yerr=fle)
+        #plt.plot(pix, utl.neg_gaus(pix, soln.x[0], soln.x[1], soln.x[2], soln.x[3]))
+        #plt.show()
+    return mid_pix
+
+p22 = os.getcwd() + '/Radial_Velocity/Results/'
+
+list1 = os.listdir(p22)
+list2 = []
+
+for i in range(len(list1)):
+    if list1[i][-12:] == '_spectra.dat':
+        list2.append(list1[i])
+
+list2.sort(key=utl.natural_keys)
+
+#print(list2)
+
+f22 = open(p22 + 'positions.dat', 'w')
+
+for j in range(len(list2)):
+    mid_pix1 = selection(p22, list2[j])
+    f22.write(str(mid_pix1[0]) + '\t' + str(mid_pix1[1]) + '\n')
+
+f22.close()
